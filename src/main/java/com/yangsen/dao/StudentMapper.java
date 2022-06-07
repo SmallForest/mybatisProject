1f49389d1461846a29953162c1b47af12018909d
smallforest <1032817724@qq.com>
2022-06-07 15:54:18 +0800

    query students

--- src/main/java/com/yangsen/dao/StudentMapper.java
+++ src/main/java/com/yangsen/dao/StudentMapper.java
@@ -1,4 +1,9 @@
 package com.yangsen.dao;
 
+import com.yangsen.pojo.Student;
+
+import java.util.List;
+
 public interface StudentMapper {
+    List<Student> getStudents();
 }
---
+++ src/test/java/com/yangsen/dao/StudentMapperTest.java
@@ -0,0 +1,29 @@
+package com.yangsen.dao;
+
+import com.yangsen.pojo.Student;
+import com.yangsen.pojo.Teacher;
+import com.yangsen.utils.MyBatisUtil;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+
+import java.util.List;
+
+public class StudentMapperTest {
+    //声明logger
+    public static final Logger logger = LogManager.getLogger();
+
+    @Test
+    public void getStudents(){
+        //获取sqlSession
+        try (SqlSession sqlSession = MyBatisUtil.getSqlSession()) {
+            //获取mapper
+            StudentMapper mapper = sqlSession.getMapper(StudentMapper.class);
+            List<Student> list = mapper.getStudents();
+            for (Student s : list) {
+                logger.info(s);
+            }
+        }
+    }
+}
